39f9b80c01e5980c2f2dcc8f24f8ab0330455133
aiki6643 <aiki6643@gmail.com>
2018-08-26 21:35:17 +0900

    Revert "Revert "Initial commit""
    This reverts commit 08eef49d54e3ad5fe50ec5729fb2cb4de1e761fd.

---
+++ NetworkCheck.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Net.NetworkInformation;
+
+namespace NetworkSpeedCheck
+{
+    public class NetworkCheck
+    {
+        string _netSpeed = string.Empty;
+        public string _NetSpeed
+        {
+            get { return _netSpeed; }
+        }
+
+        public long getRoundTime(string ip)
+        {
+            // Ping's the local machine.
+            Ping pingSender = new Ping();
+            PingReply reply = pingSender.Send(ip);
+            if (reply.Status == IPStatus.Success)
+            {
+                return reply.RoundtripTime == 0 ? 1000L : reply.RoundtripTime;
+            }
+
+            return 0L;
+        }
+    }
+}
